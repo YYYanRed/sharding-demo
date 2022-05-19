d6bf8e0367b37c2504bbf6e2eaab4d82d774a991
YYYanRed <2501074063@qq.com>
2022-05-19 19:36:27 +0800

    完成领导分配的第8个功能

--- src/main/java/com/tedu/aaaaa.java
+++ src/main/java/com/tedu/aaaaa.java
@@ -4,6 +4,7 @@ public class aaaaa {
     public static void main(String[] args) {
         System.out.println("我改我的");
         System.out.println("你改你的");
+        System.out.println("我还要改");
     }
 
 }
